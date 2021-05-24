3d8b9a1c1b0d8a57d46b5233a1b6f81b797cb752
pcliquet <pedroca8@al.insper.edu.br>
2021-05-24 11:15:19 -0300

    comitando

--- index.py
+++ index.py
@@ -1,4 +1,21 @@
 import pygame
 from config import *
-from assests import *
-from path import os
+from assets import *
+
+
+pygame.init()
+
+
+#Display
+window = pygame.display.set_mode((x_size, y_size))
+
+while game == 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = 0
+
+    window.blit(mapa_img, (0,0))
+    pygame.display.update()
+
+
+pygame.quit() 
