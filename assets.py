a8a616f4bb30c8988c7707f4283bd5916348807d
Gustavo Oliveira <gustavoeso@al.insper.edu.br>
2021-05-26 11:12:31 -0300

    adicionando classe player

--- assets.py
+++ assets.py
@@ -5,3 +5,7 @@ from path import os
 map_dir = path.join(path.dirname(__file__), 'assets/img/map')
 mapa_img = pygame.image.load(path.join(map_dir, 'Map001.png')).convert()
 mapa_img = pygame.transform.scale(mapa_img,(2580,1440))
+
+player_dir = path.join(path.dirname(__file__),'assets/img/char')
+player_img = pygame.image.load(path.join(player_dir, 'char_1.png'))
+player_img = pygame.transform.scale(player_img,(200,200))
--- classes.py
+++ classes.py
@@ -30,3 +30,28 @@ class Mapa(pygame.sprite.Sprite):
         if self.rect.bottom < y_size:
             self.rect.bottom = y_size
 
+class player(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.img = player_img
+        self.rect = self.img.get_rect()
+        self.rect.x = 520
+        self.rect.y = 360
+        self.speedx = 0
+        self.speedy = 0
+    
+    def update (self):
+        self.rect.x += self.speedx 
+        self.rect.y += self.speedy
+            
+        if self.rect.right < x_size:
+            self.rect.right = 1290
+        
+        if self.rect.left > 0:
+            self.rect.left = 0
+
+        if self.rect.top > 0:
+            self.rect.top = 0
+
+        if self.rect.bottom < y_size:
+            self.rect.bottom = y_size
