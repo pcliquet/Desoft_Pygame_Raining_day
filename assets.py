0e634916af2ebe46623cb9a0734499ce16507372
pcliquet <pedroca8@al.insper.edu.br>
2021-05-31 10:30:13 -0300

    tentando arruma o bug

--- assets.py
+++ assets.py
@@ -13,3 +13,8 @@ mapa_img = pygame.transform.scale(mapa_img,(4000,2000))
 player_dir = path.join(path.dirname(__file__),'assets/img/char')
 player_img = pygame.image.load(path.join(player_dir, 'char_1.png'))
 player_img = pygame.transform.scale(player_img,(200,200))
+
+gota_dir = path.join(path.dirname(__file__), 'assets/img/items/gota')
+gota_img = pygame.image.load(path.join(gota_dir, 'gota.png'))
+
+gota_img = pygame.transform.scale(gota_img, (50, 50)) 
--- classes.py
+++ classes.py
@@ -58,3 +58,32 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom > 790:
             self.rect.bottom = 790
         
+class Raio(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, 4000)
+        self.rect.y = random.randint(0, 2000)
+
+class Madeira(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, 4000)
+        self.rect.y = random.randint(0, 2000)
+
+    def update(self):
+        pass
+
+class Pedra(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, 4000)
+        self.rect.y = random.randint(0, 2000)
+
+    def update(self):
+        pass 
