d4ff9405b0f2eb349c22adb182e0e0058b3c1636
Gustavo Oliveira <gustavoeso@al.insper.edu.br>
2021-05-31 10:17:43 -0300

    adicionando classes de objetos

--- classes.py
+++ classes.py
@@ -2,31 +2,7 @@ from pygame.display import update
 import pygame
 from assets import *
 from config import *
-
-class Mapa(pygame.sprite.Sprite):
-    def __init__(self,image):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = mapa_img
-        self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = -720
-        self.speedx = 0
-        self.speedy = 0
-        
-    def update(self):
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-       
-
-        # Mantem dentro da tela
-        if self.rect.right < x_size:
-            self.rect.right = 1290
-        if self.rect.left > 0:
-            self.rect.left = 0
-        if self.rect.top > 0:
-            self.rect.top = 0
-
-
+import random
 
 class Mapa(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -80,4 +56,33 @@ class Player(pygame.sprite.Sprite):
 
         if self.rect.bottom > 790:
             self.rect.bottom = 790
-        
+
+class Raio(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, 4000)
+        self.rect.y = random.randint(0, 2000)
+
+class Madeira(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, 4000)
+        self.rect.y = random.randint(0, 2000)
+    
+    def update(self):
+        pass
+
+class Pedra(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, 4000)
+        self.rect.y = random.randint(0, 2000)
+    
+    def update(self):
+        pass
